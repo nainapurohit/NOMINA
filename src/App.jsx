ce129bcd9284196743fcee8c809263e31e8ae806
nainapurohit <155882208+nainapurohit@users.noreply.github.com>
2026-05-11 12:58:28 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+//import { useState } from "react";
+//import "./App.css";
+
+function App() {
+	return (
+		<div className="h-screen flex items-center justify-center bg-black text-white text-3xl font-bold">
+			<h1 className="text-5xl font-bold text-blue-500">Nomina is alive!!</h1>
+		</div>
+	);
+}
+
+export default App;
